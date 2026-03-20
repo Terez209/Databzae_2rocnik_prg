d6aef58704642162b22abe90fdad30aaf9c147c6
Terez209 <61789cenigova@student.sstebrno.cz>
2026-03-20 14:00:34 +0100

    Add files via upload

---
+++ databzae_hry/databzae_hry/hry.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace databzae_hry
+{
+    public class hry
+    {
+        public string Jmeno { get; set; }
+        public string Autor { get; set; }
+        public int RokVydani { get; set; }
+        public string Zanr { get; set; }
+        public string instalace { get; set; }
+
+    }
+}
